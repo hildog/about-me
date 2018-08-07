02981db481460b8d001c11ad457c8a6829f36403
Hilary Waite <hil.waite@gmail.com>
2018-08-07 15:33:25 -0700

    added foundation files

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict'; 
+
+// Get the user's name 
+
+var userName= prompt('Hi there! What is your name?');
+
+alert('Nice to have you here, ' + userName +' . I\'m going to ask you some fun questions about me. These are all yes/no questions, so feel f ree to answer with y/n or yes/no.'); 
+
+
+// Ask them five y/n questions
+
+var answerOne = prompt('Do I have any pets?').toLowerCase();
+if (correctAnswer) {
+
+
+
+// if (answerOne.toLowerCase() === 'y' || answerOne.toLowerCase() === 'yes') {
+alert('you got it right!');
+} else { 
+alert('FAIL');
+}
