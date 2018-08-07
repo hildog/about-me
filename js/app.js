f88a8d06f030377f706523e70a8574f0e2d7a200
Hilary Waite <hil.waite@gmail.com>
2018-08-07 16:27:05 -0700

    added more code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 'use strict'; 
 
 // Get the user's name 
@@ -10,8 +11,34 @@ alert('Nice to have you here, ' + userName +' . I\'m going to ask you some fun q
 // Ask them five y/n questions
 
 var answerOne = prompt('Do I have any pets?').toLowerCase();
-if (correctAnswer) {
+if (answerOne === 'y' || answerOne === 'yes') {
+    alert('That is correct! I have two cats.')
+} else { 
+    alert('Sorry, you got it wrong!')
 
+}
+  }
+
+  var answerTwo = prompt('Do I speak German?').toLowerCase();
+  if (answerOne === 'y' || answerOne === 'yes') {
+    alert('That is correct!') 
+}
+ else {
+     alert('Ja, Ich Spreche Deutch!')
+
+}
+
+if var answerThree = prompt('Have I lived in Florida?').toLowerCase(); 
+if (answerOne === 'n' || answerOne === 'no') {
+    alert('That is correct!')
+    else { 
+        alert('Nope, never have I lived there!')
+    }
+if var answerFour = prompt('Do I think mushrooms are delicious?').toLowerCase();
+if (answerFour === 'n' || answerFour ==='no')
+
+
+if var answerFive = prompt('Do I brew kombucha at home?').toLowerCase();
 
 
 // if (answerOne.toLowerCase() === 'y' || answerOne.toLowerCase() === 'yes') {
